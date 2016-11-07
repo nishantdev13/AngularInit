d71f18abd2c8b011761f251c81d089499fd2a8e9
nishantdev13 <nishantdev13@gmail.com>
2016-11-07 09:44:22 +0530

    Bind with object

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 
+export class Hero {
+    id: number;
+    name: string;
+}
+
 @Component({
     selector: 'my-app',
-    template: `<h1>{{title}}</h1><h2>{{hero}} details!</h2>`
+    template: `
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} details!</h2>
+    <div><label>id: </label>{{hero.id}}</div>   
+    `
 })
 
 export class AppComponent {
     title = 'Tour of Heroes';
-    hero = 'Windstorm';
-
+    hero: Hero = {
+        id: 1,
+        name: 'Windstorm'
+    };
 }
